77e481a90dc112ca7406fc88856f81e70688371b
faisal khan <ffaisalkhan78@gmail.com>
2023-09-08 15:25:58 +0530

    Edit On Programme

---
+++ 01Basic/Variables_.js
@@ -0,0 +1,15 @@
+const accountId= 22334455
+let accountEmail='Fkf@gmail.com'
+var accountPassword ='12345'
+accountCity='Mumbai';
+
+accountEmail="Fkkkkhan@gmail.com"
+accountPassword='1234789'
+accountCity='Pune'
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+/*
+Prefer Not To Use Var KeyWord Because of Block Scope and Functional Scope
+*/
